c3eb49e0486cf9b45aff76f4cf151f0211c5b3ae
Brenda Padilha <bpadilhaferreira@gmail.com>
2024-09-15 12:35:57 -0300

    feat: modal de opções de cadastro

--- app/(public)/index.jsx
+++ app/(public)/index.jsx
@@ -1,17 +1,26 @@
+import React, {useState} from 'react';
 import { Container, ContainerButton, Logo, Title, SubTitle, Button, LinkButton, LinkStyled } from "@/styles/index";
+import SignInOptions from "@/components/sign-in-options";
 
 const ImageLogo = require('@/assets/images/logo.png');
 
 export default function WelcomePage() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <Container>
       <Logo source={ImageLogo} />
       <Title>Bem Vindo ao seu app de tratamento diário!</Title>
       <SubTitle>Aqui iremos ajuda-lo a se desenvolver tanto psicologicamente quanto em sua cordenação motora.</SubTitle>
       <ContainerButton>
-        <Button>Cadastrar</Button>
+        <Button onPress={() => setModalVisible(true)}>Cadastrar</Button>
         <LinkButton href="/(public)/sign-in">Entrar</LinkButton>
       </ContainerButton>
+      <SignInOptions
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
       <LinkStyled href="/">Termos de serviços</LinkStyled>
     </Container>
   )
---
+++ app/(public)/sign-up-child.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SignUpChild() {
+  return (
+    <View>
+      <Text>sign-in-child</Text>
+    </View>
+  )
+}

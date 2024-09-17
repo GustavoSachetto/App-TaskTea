9ed5d24eae605cf59f76a65915c5fcd257055ac4
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-09-17 10:43:33 -0300

    feat: scroll view na sign-in

--- app/(public)/sign-in.tsx
+++ app/(public)/sign-in.tsx
@@ -1,5 +1,6 @@
 import { router } from 'expo-router';
 import { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { Container, Logo, Title, SubTitle, Border, Input, Line, ContainerButtonsSignIn, ButtonSignIn, LinkStyled, TextButton } from "@/styles/index";
 import { useSession } from '@/hooks/ctx';
 
@@ -12,35 +13,37 @@ export default function SignIn() {
   const [password, setPassword] = useState('');
   
   return (
-    <Container>
-      <Logo source={ImageLogo} resizeMode="contain"/>
-      <Title>Bem-vindo de volta!</Title>
-      <SubTitle>Ficamos muito felizes com seu empenho e vontade de prosseguir com seus desafios.</SubTitle>
-      <Border customColor={Colors.colors.green}>
-        <Title customColor={Colors.title.green} style={{padding:10}}>Entrar</Title>
-        <Line customColor={Colors.colors.green} />
-        <Input 
-          customColor={Colors.colors.green} 
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          placeholder='Email' />
-        <Input 
-          customColor={Colors.colors.green}
-          placeholder='Senha' 
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          secureTextEntry />
-        <ContainerButtonsSignIn>
-          <ButtonSignIn customColor={Colors.colors.red}>
-            <TextButton>Voltar</TextButton>
-          </ButtonSignIn>
-          <ButtonSignIn customColor={Colors.colors.green}>
-            <TextButton>Entrar</TextButton>
-          </ButtonSignIn>
-        </ContainerButtonsSignIn>
-      </Border>
-      <LinkStyled href="/">Termos de serviços</LinkStyled>
-    </Container>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <Container>
+        <Logo source={ImageLogo} resizeMode="contain"/>
+        <Title>Bem-vindo de volta!</Title>
+        <SubTitle>Ficamos muito felizes com seu empenho e vontade de prosseguir com seus desafios.</SubTitle>
+        <Border customColor={Colors.colors.green}>
+          <Title customColor={Colors.title.green} style={{padding:10}}>Entrar</Title>
+          <Line customColor={Colors.colors.green} />
+          <Input 
+            customColor={Colors.colors.green} 
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            placeholder='Email' />
+          <Input 
+            customColor={Colors.colors.green}
+            placeholder='Senha' 
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry />
+          <ContainerButtonsSignIn>
+            <ButtonSignIn customColor={Colors.colors.red}>
+              <TextButton>Voltar</TextButton>
+            </ButtonSignIn>
+            <ButtonSignIn customColor={Colors.colors.green}>
+              <TextButton>Entrar</TextButton>
+            </ButtonSignIn>
+          </ContainerButtonsSignIn>
+        </Border>
+        <LinkStyled href="/">Termos de serviços</LinkStyled>
+      </Container>
+    </ScrollView>
   );
 }
 

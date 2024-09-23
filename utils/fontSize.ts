c8586df75d1468d1bba088e977617d1f9a4b132e
Brenda Padilha <bpadilhaferreira@gmail.com>
2024-09-23 18:27:05 -0300

    fix: corrigindo função de medidas

--- utils/fontSize.ts
+++ utils/fontSize.ts
@@ -1,10 +1,10 @@
 import { Dimensions } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const { width } = Dimensions.get('window');
+const width = Dimensions.get('screen').width;
 
 export const getFontSize = (size : number) => {
-  if (width > 500) { 
+  if (width > 768) { 
     return wp(size * 0.35); 
   }
   return wp(size * 0.5); 
--- utils/responsiveMesures.ts
+++ utils/responsiveMesures.ts
@@ -1,17 +1,17 @@
 import { Dimensions } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const { width } = Dimensions.get('window');
+const width = Dimensions.get('screen').width;
 
 export const h = (size : number) => {
-  if (width > 500) { 
+  if (width > 768) { 
     return hp(size * 0.8); 
   }
   return hp(size * 1); 
 };
 
 export const w = (size : number) => {
-    if (width > 500) { 
+    if (width > 768) { 
       return wp(size * 0.8); 
     }
     return wp(size * 1); 

85574d9f9dacbf3c60ad47157109910cd715fca9
Gustavo Sachetto <g.sachettocruz@gmail.com>
2024-09-24 19:36:51 -0300

    feat: implementa as funções de usuário da api

---
+++ services/api/user.ts
@@ -0,0 +1,111 @@
+import { api } from "@/services/api";
+
+type UserProps = {
+  id: number,
+  name: string,
+  email: string,
+  nickname: string,
+  age: number,
+  cpf?: string,
+  role?: Array<string>,
+  phone_number?: string,
+  created_at: string,
+  updated_at: string
+}
+
+type UserRelationshipProps = {
+  id: number,
+  name: string,
+  email: string,
+  nickname: string,
+  age: number,
+  image: string | null,
+  created_at: string,
+  updated_at: string
+}
+
+type PostUserChildProps = {
+  name: string,
+  email: string,
+  nickname: string,
+  password: string,
+  age: number
+}
+
+type PostUserResponsibleProps = PostUserChildProps & {
+  cpf: string,
+  phone_number: string,
+}
+
+type PutUserProps = {
+  name: string,
+  email: string,
+  nickname: string,
+  age?: number,   
+  current_password: string,
+  new_password: string | null,
+  new_password_confirmation: string | null,
+}
+
+export const getMyUser = async (token: string) => {
+  const response = await api.get<UserProps>(
+    `/users`,{
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 
+
+export const getMyRelationships = async (token: string) => {
+  const response = await api.get<UserRelationshipProps[]>(
+    `/users/relationship`,{
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 
+
+export const createUserChild = async (data: PostUserChildProps, token: string) => {
+  const response = await api.post<UserProps>(
+    `/users/child`, data,{
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 
+
+export const createUserResponsible = async (data: PostUserResponsibleProps, token: string) => {
+  const response = await api.post<UserProps>(
+    `/users/responsible`, data,{
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 
+
+export const createUserRelationship = async (userId: number, token: string) => {
+  const response = await api.post<{ message: string }>(
+    `/users/relationship`, { userId },{
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 
+
+export const editMyUser = async (data: PutUserProps, token: string) => {
+  const response = await api.put<UserProps>(
+    `/users`, data,{
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 
+
+export const deleteMyUser = async (token: string) => {
+  const response = await api.delete<{ message: string }>(
+    `/users`, {
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+
+  return response.data;
+} 

2610da38282326ffef88181dbeba8391bd5802f1
Brenda Padilha <bpadilhaferreira@gmail.com>
2024-10-21 13:13:01 -0300

    fix: underline na barra de navegação da child

--- app/(auth)/(child)/(tabs)/_layout.tsx
+++ app/(auth)/(child)/(tabs)/_layout.tsx
@@ -1,14 +1,13 @@
 import { Tabs } from 'expo-router';
-import { Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import { getMyUser, UserProps } from '@/services/api/routes/user';
 import { useEffect, useState } from 'react';
 import { useSession } from '@/hooks/ctx';
 import { Overlay } from '@/styles';
 import { useOverlay } from '@/context/OverlayContext';
-import { View } from '@/styles/index-responsible';
+import { h, w } from '@/utils/responsiveMesures';
 
 export default function Layout() {
-  const [activeTab, setActiveTab] = useState<string>('index');
   const [userData, setUserData] = useState<UserProps | undefined>(undefined);
   const { session } = useSession();
   const { isVisible } = useOverlay(); 
@@ -24,70 +23,52 @@ export default function Layout() {
     }
   };
 
-  const renderTabIcon = (name: string, source: any) => {
-    return (
-      <View style={styles.tabContainer}>
-        <Image source={source} style={styles.icon} />
-        {activeTab === name && <View style={styles.underline} />} {/* Sub-linha */}
-      </View>
-    );
-  };
-
   return (
     <>
       {isVisible && <Overlay />}
       <Tabs
-        screenOptions={{
+        screenOptions={({ route }) => ({
           headerShown: false,
           tabBarShowLabel: false,
-          tabBarButton: (props) => {
-            const { route, onPress } = props;
+          tabBarIcon: ({ focused }) => {
+            let iconSource;
+            let customStyle = styles.icon; 
+            let resize = "contain"; 
+
+            switch (route.name) {
+              case 'index':
+                iconSource = require('../../../../assets/icons/casa.png');
+                break;
+              case 'tasks':
+                iconSource = require('../../../../assets/icons/quebra-cabeca.png');
+                break;
+              case 'profile':
+                iconSource = userData?.image
+                  ? { uri: userData.image }
+                  : require('../../../../assets/icons/perfil.png');
+                customStyle = styles.profileContainer;
+                resize = "cover"; 
+                break;
+              case 'settings':
+                iconSource = require('../../../../assets/icons/configuracao.png');
+                break;
+            }
+
             return (
-              <TouchableWithoutFeedback onPress={() => {
-                setActiveTab(route.name); // Atualiza a aba ativa
-                if (onPress) onPress(); // Chama a função de onPress se estiver definida
-              }}>
-                {props.children}
-              </TouchableWithoutFeedback>
+              <View style={styles.tabContainer}>
+                <View style={customStyle}>
+                  <Image source={iconSource} style={styles.icon} resizeMode={resize}  />
+                </View>
+                {focused && <View style={styles.underline} />}
+              </View>
             );
           },
-        }}
+        })}
       >
-        <Tabs.Screen
-          name="index"
-          options={{
-            tabBarIcon: () => renderTabIcon('index', require('../../../../assets/icons/casa.png'))
-          }}
-        />
-        <Tabs.Screen
-          name="tasks"
-          options={{
-            tabBarIcon: () => renderTabIcon('tasks', require('../../../../assets/icons/quebra-cabeca.png'))
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            tabBarIcon: () => (
-              <Image
-                source={userData?.image ? { uri: userData.image } : require('../../../../assets/icons/perfil.png')}
-                style={{ width: 30, height: 30, borderRadius: 100 }}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: "Settings",
-            tabBarIcon: () => (
-              <Image
-                source={require('../../../../assets/icons/configuracao.png')}
-                style={{ width: 30, height: 30 }}
-              />
-            ),
-          }}
-        />
+        <Tabs.Screen name="index" />
+        <Tabs.Screen name="tasks" />
+        <Tabs.Screen name="profile" />
+        <Tabs.Screen name="settings" />
       </Tabs>
     </>
   );
@@ -99,13 +80,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   icon: {
-    width: 30,
-    height: 30,
+    width: w(8),
+    height: w(8),
+  },
+  profileContainer: {
+    width: w(8),
+    height: w(8),
+    borderRadius: w(8), 
+    overflow: 'hidden', 
   },
   underline: {
     width: '100%',
     height: 2,
-    backgroundColor: 'blue', // Cor do sublinhado
-    marginTop: 4, // Espaço entre o ícone e o sublinhado
+    backgroundColor: 'blue',
+    marginTop: 4,
   },
 });

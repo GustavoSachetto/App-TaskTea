4b72248755881edad8752f07b0385edf3eda2771
Brenda Padilha <bpadilhaferreira@gmail.com>
2024-10-23 19:43:19 -0300

    feat: input e data de nascimento em cadastro

--- app/(public)/sign-up-child.tsx
+++ app/(public)/sign-up-child.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native';
 import { useState } from 'react';
 import {
   Border, Input, Line, ContainerButtonsSign, ButtonSign, LinkedSign,
@@ -18,26 +18,26 @@ import { createUserChild } from '@/services/api/routes/user';
 import ServiceTerms from '@/components/service-terms';
 import { Ionicons } from '@expo/vector-icons';
 import { h, w } from '@/utils/responsiveMesures';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const GrayColor = Colors.colors.gray;
 const GreenColor = Colors.colors.green;
 
 export default function SignUpChild() {
   const [modalVisible, setModalVisible] = useState(false);
-
-  let bouncyCheckboxRef: typeof BouncyCheckbox | null = null;
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
-  const [age, setAge] = useState('');
+  const [birthDate, setBirthDate] = useState(new Date()); 
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [showDatePicker, setShowDatePicker] = useState(false);
 
   const toggleShowPassword = () => {
-      setShowPassword(!showPassword);
+    setShowPassword(!showPassword);
   };
 
   const handleSignUp = async () => {
@@ -45,24 +45,25 @@ export default function SignUpChild() {
       name,
       email,
       nickname,
+      birthdate: `${birthDate.getFullYear()}/${String(birthDate.getMonth()).padStart(2, '0')}/${String(birthDate.getDate()).padStart(2, '0')}`, 
       password,
-      age: Number(age),
     };
     setLoading(true);
     const response = await createUserChild(userData);
     setLoading(false);
-    setIsCheckboxChecked(false)
-    if (response)
+    setIsCheckboxChecked(false);
+    if (response) {
       Toast.show({
         text1: 'Mensagem',
         text2: response.message
       });
+    }
   };
 
   return (
-    <>{modalVisible && <Overlay />}
+    <>
+      {modalVisible && <Overlay />}
       <ContainerScrollView>
-
         {loading ? (
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <ActivityIndicator size="large" color={GreenColor} />
@@ -89,15 +90,24 @@ export default function SignUpChild() {
                 onChangeText={(text: string) => setNickname(text)}
               />
               <Input
-                placeholder='Idade:'
-                placeholderTextColor={GrayColor}
+                value={birthDate.toLocaleDateString("pt-BR")} 
+                onFocus={() => setShowDatePicker(true)}
+                placeholder="Data de nascimento"
+                editable={true} 
                 customColor={GreenColor}
-                value={age}
-                onChangeText={(text: string) => {
-                  const numericValue = text.replace(/[^0-9]/g, '');
-                  setAge(numericValue.slice(0, 2))
-                }}
               />
+              {showDatePicker && (
+                <DateTimePicker
+                  value={birthDate}
+                  mode="date"
+                  display="default"
+                  onChange={(event, selectedDate) => {
+                    const currentDate = selectedDate || birthDate; 
+                    setShowDatePicker(false); 
+                    setBirthDate(currentDate); 
+                  }}
+                />
+              )}
               <Input
                 placeholder='E-mail:'
                 placeholderTextColor={GrayColor}
@@ -106,26 +116,26 @@ export default function SignUpChild() {
                 onChangeText={(text: string) => setEmail(text)}
               />
               <PasswordContainer>
-              <InputWrapper customColor={GreenColor}>
-              <InputPassword
-                placeholder='Senha:'
-                placeholderTextColor={GrayColor}
-                customColor={GreenColor}
-                value={password}
-                onChangeText={(text: string) => setPassword(text)}
-                secureTextEntry={!showPassword}
-              />
-               <Ionicons
-          name={showPassword ? "eye-off" : "eye"}
-          onPress={toggleShowPassword}
-          color="#808080"
-          size={w(5)} 
-        />
-              </InputWrapper>
+                <InputWrapper customColor={GreenColor}>
+                  <InputPassword
+                    placeholder='Senha:'
+                    placeholderTextColor={GrayColor}
+                    customColor={GreenColor}
+                    value={password}
+                    onChangeText={(text: string) => setPassword(text)}
+                    secureTextEntry={!showPassword}
+                  />
+                  <Ionicons
+                    name={showPassword ? "eye-off" : "eye"}
+                    onPress={toggleShowPassword}
+                    color="#808080"
+                    size={w(5)} 
+                  />
+                </InputWrapper>
               </PasswordContainer>
               <ContainerRow>
                 <BouncyCheckbox
-                  ref={bouncyCheckboxRef}
+                  ref={BouncyCheckbox}
                   disableText
                   fillColor="#46f87c"
                   size={30}
@@ -162,5 +172,5 @@ export default function SignUpChild() {
           onClose={() => setModalVisible(false)} />
       </ContainerScrollView>
     </>
-  )
+  );
 }
